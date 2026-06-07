1976eb2dae3c172a1e51c192242103c2d8aa7d43
Atharv Kulkarni <atharv.kulkarni@solytics-partners.com>
2026-06-07 09:27:50 +0530

    Add SonarQube test files

---
+++ bad.py
@@ -0,0 +1,15 @@
+def test():
+    password = "admin123"   # hardcoded secret
+    x = 1
+    y = 2
+    z = 3
+    a = 4
+    b = 5
+    c = 6
+    d = 7
+    e = 8
+    f = 9
+    g = 10
+    print(password)
+
+test()
